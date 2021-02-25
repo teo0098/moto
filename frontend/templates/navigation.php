9e50ed7ef69f6b2934d3a4ff1ffecd567f6cb03a
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2021-02-25 12:45:49 +0100

    Create moto.pl project structure in php

---
+++ frontend/templates/navigation.php
@@ -0,0 +1,14 @@
+<header class="logo">
+    <section>
+        <h1>MOTO.PL</h1>
+    </section>
+    <nav>
+        <ul>
+            <li><a href="../views/">Strong główna</a></li>
+            <li><a href="../views/watched.php">Obserwowane</a></li>
+            <li><a href="../views/signin.php">Zaloguj</a></li>
+            <li><a href="../views/signup.php">Zarejestruj</a></li>
+            <li><a href="../views/postOffer.php">Wystaw oferte</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ frontend/views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/navigation.css">
+    <link rel="stylesheet" href="../styles/global.css">
+    <title>Moto.pl</title>
+</head>
+<body>
+    <?php include "../templates/navigation.php" ?>
+</body>
+</html>
