fb3126f3ebbe8799958dea2987ad3a142beaaad6
Dezerow <darth3160@gmail.com>
2021-03-22 18:27:57 +0100

    added site logo fixed code(added new section - header, now we have working sticky-top navbar)

--- frontend/views/signin.php
+++ frontend/views/signin.php
@@ -1,8 +1,9 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <meta charset="UTF-8">
@@ -13,42 +14,47 @@
     <link rel="stylesheet" href="../styles/signin.css">
     <title>Moto.pl</title>
 </head>
+
+<header class="sticky-top">
+    <?php include "../templates/navigation.php" ?>
+</header>
+
 <body>
-    <?php include "../templates/navigation.php" 
+    <?php
 
-//    $emailErr = $pwdErr = $rememberErr = '';
-//    $email = $pwd = $remember = '';
+    //    $emailErr = $pwdErr = $rememberErr = '';
+    //    $email = $pwd = $remember = '';
 
-//    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])){
-//        if (empty($_POST['email'])){
-//            $emailErr = 'Email is required!';
-//        }else{
-//            $email = validate($_POST['email']);
-            //check if email address is well form
-//            if (!filter_var($email,FILTER_VALIDATE_EMAIL)){
-//                $emailErr = 'Invalid email format!';
-//            }
-//        }
+    //    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])){
+    //        if (empty($_POST['email'])){
+    //            $emailErr = 'Email is required!';
+    //        }else{
+    //            $email = validate($_POST['email']);
+    //check if email address is well form
+    //            if (!filter_var($email,FILTER_VALIDATE_EMAIL)){
+    //                $emailErr = 'Invalid email format!';
+    //            }
+    //        }
 
-//        if (empty($_POST['pwd'])){
-//            $pwdErr = 'Password is required!';
-//        }else{
-//            $pwd = validate($_POST['pwd']);
-//        }
+    //        if (empty($_POST['pwd'])){
+    //            $pwdErr = 'Password is required!';
+    //        }else{
+    //            $pwd = validate($_POST['pwd']);
+    //        }
 
-//        if (empty($_POST['remember'])) {
-//            $remember = '';
-//        }else{
-//            $remember = validate($_POST['remember']);
-//        }
-//    }
+    //        if (empty($_POST['remember'])) {
+    //            $remember = '';
+    //        }else{
+    //            $remember = validate($_POST['remember']);
+    //        }
+    //    }
 
-//    function validate($data){
-//        $data = trim($data);
-//        $data = stripcslashes($data);
-//        $data = htmlspecialchars($data);
-//        return $data;
-//    }
+    //    function validate($data){
+    //        $data = trim($data);
+    //        $data = stripcslashes($data);
+    //        $data = htmlspecialchars($data);
+    //        return $data;
+    //    }
     ?>
     <div class="container">
         <div class="card card-container">
@@ -56,12 +62,12 @@
             <p id="profile-name" class="profile-name-card"></p>
             <form class="form-signin" method="POST" action="../../backend/server/login.php">
                 <?php
-                    if (isset($_SESSION['loginError'])) {
-                        echo '<div class="alert alert-danger" role="alert">
-                                    '.$_SESSION['loginError'].'
+                if (isset($_SESSION['loginError'])) {
+                    echo '<div class="alert alert-danger" role="alert">
+                                    ' . $_SESSION['loginError'] . '
                                 </div>';
-                    }
-                    $_SESSION['loginError'] = null;
+                }
+                $_SESSION['loginError'] = null;
                 ?>
                 <span id="reauth-email" class="reauth-email"></span>
                 <input name='email' type="email" id="inputEmail" class="form-control" placeholder="Adres Email" required autofocus>
@@ -86,4 +92,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/3810206ae2.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
