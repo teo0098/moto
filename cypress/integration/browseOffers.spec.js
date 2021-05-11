b710b79a1fda7d777ad7d95525195416b9ee7a9d
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2021-05-11 16:58:17 +0200

    Prepare cypress tests

--- cypress/integration/browseOffers.spec.js
+++ cypress/integration/browseOffers.spec.js
@@ -6,11 +6,14 @@ describe("Browse offers", () => {
   it("Browses offers", () => {
     cy.get('[data-test-id="searchShort"]').click({ force: true });
     cy.url().should("include", "/offers.php?");
+<<<<<<< HEAD
     cy.get('[data-test-id="page2"]').click({ force: true });
     cy.url().should("include", "page=2");
     cy.get('[data-test-id="page3"]').click({ force: true });
     cy.url().should("include", "page=3");
     cy.get('[data-test-id="offerNr0"]').click({ force: true });
     cy.url().should("include", "/offer.php?id=");
+=======
+>>>>>>> 7f74803... Prepare cypress tests
   });
 });
