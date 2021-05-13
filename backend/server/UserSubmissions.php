a8fda599bb34c01e2bc2b539974783e276b54d9e
Dezerow <darth3160@gmail.com>
2021-05-13 10:38:30 +0200

    add Contact Page front + back

--- backend/server/UserSubmissions.php
+++ backend/server/UserSubmissions.php
@@ -7,15 +7,24 @@ include "../db/dbCredentials.php";
 $subject = $_POST['subject'];
 $userEmail = $_POST['email'];
 $problemDesc = $_POST['problemDesc'];
-$username = $_POST['username'];
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+
+if (!filter_var($userEmail, FILTER_VALIDATE_EMAIL) || !preg_match('/^[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ\d\s]{5,20}$/', $subject) 
+|| !preg_match('/^[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ\d\s]{20,100}$/', $problemDesc) || !preg_match('/^[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ\s]{3,20}$/', $name)
+|| !preg_match('/^[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ\s]{3,20}$/', $surname)){
+    $_SESSION["Submission"] = "Wypełnij wszystkie pola tekstowe";
+    header("Location: ../../frontend/views/contactHelp.php");
+}else{
+
 
 $email = new \SendGrid\Mail\Mail();
 $email->setFrom("agnachel0098@gmail.com", "Moto.pl");
 $email->setSubject($subject);
-$email->addTo($userEmail, $username);
+$email->addTo("agnachel0098@gmail.com", $username);
 $email->addContent(
     "text/html",
-    "<p>Użytkownik: $username <br><br> $problemDesc </p>"
+    "<p>Użytkownik: $name $surname<br><br> E-mail użytkownika: $userEmail <br><br>$problemDesc</p>"
 );
 $sendgrid = new \SendGrid($GLOBALS['MOTO_PL_SENDGRID_KEY']);
 
@@ -25,10 +34,11 @@ try {
         $_SESSION["Submission"] = "Zgłoszenie zostało wysłane poprawnie";
         header("Location: ../../frontend/views/contactHelp.php");
     }else{
-        $_SESSION["Submission"] = "Wystąpił błąd przy wysyłaniu zgłoszenia";
+        throw new Exception();
     }
 } catch (Exception $e) {
-    return false;
+    $_SESSION["Submission"] = "Wystąpił błąd przy wysyłaniu zgłoszenia";
+    header("Location: ../../frontend/views/contactHelp.php");
 }
-
+}
 ?>
